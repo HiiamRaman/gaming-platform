bb267563a3277b94de7f0fb41983f2299f7484e9
Raman Singh <ramansinghchauhan787@gmail.com>
2026-09-02 13:18:09 +0545

    feat:setup prisma with postgreSql

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,7 @@
-import app from './app.js'
-const  PORT = process.env.PORT || 8000
+import app from "./app.js";
 
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
