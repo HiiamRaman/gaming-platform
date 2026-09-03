ab83d30178e8208fac22f39dfdac08b511caf692
Raman Singh <ramansinghchauhan787@gmail.com>
2026-09-03 11:26:16 +0545

    chore:remove backend workspace

--- apps/backend/src/routes/auth.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { registerUserController,loginController } from "../controllers/auth.controller.js";
-const router = Router();
-router.post("/register", registerUserController);
-router.post('/login',loginController)
-export default router;
